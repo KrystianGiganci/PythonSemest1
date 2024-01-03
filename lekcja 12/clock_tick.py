55d64c1a955a09ca031f6cc5a98505c26952adb8
Krystian Piszczela <01168907@pw.edu.pl>
2024-01-03 19:34:59 +0100

    gotowy szablon

---
+++ lekcja 12/clock_tick.py
@@ -0,0 +1,37 @@
+import pygame  # Import modułu
+pygame.init()  # Inicjalizacja modułu
+
+# Utworzenie okna o określonych wymiarach
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# Nadanie nazwy oknu
+pygame.display.set_caption('Pierwsza Gra')
+
+# Utworzenie zegara, który nadzoruje stałe wartości fps
+clock = pygame.time.Clock()
+
+# Zmienna określająca, czy należy zamknąć okno
+game_status = True
+
+# Kod wykonywany póki aplikacja jest uruchomiona
+while game_status:
+    events = pygame.event.get()
+    for event in events:
+        # Odkomentuj tę linijkę by zobaczyć co jest rejestrowane przez komputer
+        # kiedy ruszasz myszką, klikasz klawisz na klawiaturze
+        print(event)
+
+    # Sprawdzenie, czy należy zamknąć aplikację
+    # Czy kliknięto na X
+    if event.type == pygame.QUIT:
+        game_status = False
+        pass
+
+    pygame.display.update()
+    clock.tick(60)
+    pass
+
+pygame.quit()  # Zamknięcie aplikacji
+quit()  # Zamknięcie skryptu
